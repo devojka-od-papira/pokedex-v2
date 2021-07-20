f4d40b509052c4c4982c3ca4cfe805b2076ad78a
Bojana Bojic <bojanabojic30@gmail.com>
2021-07-20 10:19:03 +0200

    Added routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../views/homePage/index';
+import Detail from '../views/detailPage';
+import NotFound from '../views/notFoundPage';
+import '../style/reset.css';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/detail" component={Detail} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
