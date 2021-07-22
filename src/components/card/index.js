53208ec68285580f878cb4fde9655a5d69c00b4e
Bojana Bojic <bojanabojic30@gmail.com>
2021-07-22 21:52:42 +0200

    User can see the picture of the pokemon in the card and it basic data

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -4,9 +4,48 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import { CardMedia } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
 import { fetchPokemon } from '../../services/pokemon';
 
 const useStyles = makeStyles({
+  wrap: {
+    padding: 20,
+    margin: 30,
+    maxWidth: 250,
+  },
+  areaCard: {
+    width: 250,
+    height: 250,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  chip: {
+    padding: 5,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  parag: {
+    marginRight: 5,
+  },
+  image: {
+    width: 120,
+    height: 120,
+    border: '1px solid #e6e2d3',
+    borderRadius: 5,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 7,
+    boxSizing: 'border-box',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  name: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: 24,
+    lineHeight: 2,
+  },
 });
 function PokemonCard({ name, url }) {
   const classes = useStyles();
@@ -20,23 +59,57 @@ function PokemonCard({ name, url }) {
   }, []);
   console.log('pokemon5', pokemon);
 
-  function Capitalize(string) {
+  function capitalize(string) {
     return string && string[0].toUpperCase() + string.slice(1);
   }
 
   return (
-    <div>
-      <Card>
-        <CardActionArea>
+    <Card className={classes.wrap}>
+      <CardActionArea className={classes.areaCard}>
+        <div>
+          <Typography className={classes.name}>{capitalize(name)}</Typography>
+          <CardMedia className={classes.image}>
+            <img src={pokemon?.sprites?.front_shiny} alt="pokemon" />
+          </CardMedia>
           <div>
-            <Typography>{Capitalize(name)}</Typography>
-            <CardMedia>
-              <img src={pokemon?.sprites?.front_shiny} alt="pokemon" />
-            </CardMedia>
+            <div className={classes.chip}>
+              <p className={classes.parag}>
+                Type :
+              </p>
+              {pokemon?.types.map((type) => (
+                <div>
+                  <Chip
+                    variant="outlined"
+                    size="small"
+                    label={capitalize(type.type.name)}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className={classes.chip}>
+              <p className={classes.parag}>
+                Weight :
+              </p>
+              <Chip
+                variant="outlined"
+                size="small"
+                label={`${Math.round(pokemon?.weight * 0.1)} kg`}
+              />
+            </div>
+            <div className={classes.chip}>
+              <p className={classes.parag}>
+                Height :
+              </p>
+              <Chip
+                variant="outlined"
+                size="small"
+                label={`${pokemon?.height * 10} cm`}
+              />
+            </div>
           </div>
-        </CardActionArea>
-      </Card>
-    </div>
+        </div>
+      </CardActionArea>
+    </Card>
   );
 }
 
--- src/views/homePage/index.js
+++ src/views/homePage/index.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { makeStyles } from '@material-ui/core/styles';
 import PokemonCard from '../../components/card';
 import SearchBar from '../../components/searchBar';
 import { fetchPokemons } from '../../redux/actions';
 
+const useStyles = makeStyles({
+  wrap: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+});
+
 function Home({ fetchPokemonsAction, pokemonsData }) {
+  const classes = useStyles();
   useEffect(() => {
     fetchPokemonsAction();
   }, []);
@@ -13,14 +23,16 @@ function Home({ fetchPokemonsAction, pokemonsData }) {
   return (
     <div>
       <SearchBar />
-      {pokemonsData.map((pokemon) => (
-        <PokemonCard
-          key={pokemon.name}
-          pokemonsData={pokemonsData}
-          name={pokemon.name}
-          url={pokemon.url}
-        />
-      ))}
+      <div className={classes.wrap}>
+        {pokemonsData.map((pokemon) => (
+          <PokemonCard
+            key={pokemon.name}
+            pokemonsData={pokemonsData}
+            name={pokemon.name}
+            url={pokemon.url}
+          />
+        ))}
+      </div>
     </div>
   );
 }
