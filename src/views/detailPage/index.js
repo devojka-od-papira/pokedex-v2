27a5d4c7f63279cb59e798966ba39ed091211eb6
Bojana Bojic <bojanabojic30@gmail.com>
2021-07-26 22:08:15 +0200

    Modified details information about pokemon

--- src/views/detailPage/index.js
+++ src/views/detailPage/index.js
@@ -4,11 +4,40 @@ import { useParams } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import PokemonCard from '../../components/card';
+import TableContainer from '@material-ui/core/TableContainer';
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import { TableCell } from '@material-ui/core';
 import { fetchPokemonDetailAction } from '../../redux/actions';
 import { capitalize } from '../../utils';
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  image: {
+    width: 120,
+    height: 120,
+
+  },
+  imageWrap: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  name: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: 24,
+    lineHeight: 2,
+  },
+  container: {
+    width: '70%',
+    margin: 'auto',
+
+  },
+
+});
 
 function Detail({ fetchPokemonDetail, pokemonDetail }) {
   const classes = useStyles();
@@ -22,10 +51,39 @@ function Detail({ fetchPokemonDetail, pokemonDetail }) {
     <>
       <div>
         <Typography className={classes.name}>{capitalize(pokemonDetail?.name)}</Typography>
-        <img src={pokemonDetail?.sprites?.front_default} alt="pokemon_front_default" />
-        <img src={pokemonDetail?.sprites?.back_shiny} alt="pokemon_back_shiny" />
-        <img src={pokemonDetail?.sprites?.back_default} alt="pokemon_back_default" />
-        <img src={pokemonDetail?.sprites?.front_shiny} alt="pokemon_front_shiny" />
+        <div className={classes.imageWrap}>
+          <img className={classes.image} src={pokemonDetail?.sprites?.front_default} alt="pokemon_front_default" />
+
+        </div>
+      </div>
+      <div>
+        <TableContainer className={classes.container}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Weight [ kg ]</TableCell>
+                <TableCell align="right">Height [ cm ]</TableCell>
+                <TableCell align="right">Order</TableCell>
+                <TableCell align="right">Abilities</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableCell>{pokemonDetail?.id}</TableCell>
+              <TableCell align="right">{capitalize(pokemonDetail?.name)}</TableCell>
+              <TableCell align="right">{Math.round(pokemonDetail?.weight * 0.1)}</TableCell>
+              <TableCell align="right">{pokemonDetail?.height * 10}</TableCell>
+              <TableCell align="right">{pokemonDetail?.order}</TableCell>
+              <TableCell align="right">
+                {pokemonDetail?.abilities.map((ability) => (
+                  <p key={ability.ability.name}>{capitalize(ability.ability.name)}</p>
+                ))}
+              </TableCell>
+              <TableCell />
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </>
   );
